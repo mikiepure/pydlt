08a8b2bb760e1e1134fddac59a55179ab6a7da33
Miki, Hiromitsu <mikiepure+dev@gmail.com>
2022-09-10 23:07:57 +0900

    Add and fix scripts for development on Windows

--- dev/Setup.ps1
+++ dev/Setup.ps1
@@ -1,22 +1,22 @@
-. ".\Common.ps1"
+. ".\_Common.ps1"
 
 Set-Location ".."
 
-Get-Command py > $null
+Get-Command py *> $null
 if (!$?) {
     Stop-Shell "'py' is required to use the script; Please install it" 1
 }
-Get-Command poetry > $null
+Get-Command poetry *> $null
 if (!$?) {
     Stop-Shell "'poetry' is required to use the script; Please install it" 1
 }
 
 if (Test-Path ".venv") {
-    Write-Output "Python virtual environment is already created"
+    Write-Host "Python virtual environment is already created"
     $yesno = Read-Host "Do you want to re-create it? [y/N]"
     switch ($yesno) {
         "y" {
-            Write-Output "Removing python virtual environment..."
+            Write-Host "Removing python virtual environment..."
             Remove-Dir ".venv"
             if (Test-Path ".venv") {
                 Stop-Shell "Failed to remove python virtual environment" 1
@@ -28,13 +28,13 @@ if (Test-Path ".venv") {
     }
 }
 
-Write-Output "Creating python virtual environment..."
+Write-Host "Creating python virtual environment..."
 py -m venv ".venv"
 if ($LastExitCode -ne 0) {
     Stop-Shell "Failed to create python virtual environment by error: $LastExitCode" 1
 }
 
-Write-Output "Installing python required packages..."
+Write-Host "Installing python required packages..."
 poetry install
 if ($LastExitCode -ne 0) {
     Stop-Shell "Failed to install python required packages by error: $LastExitCode" 1
